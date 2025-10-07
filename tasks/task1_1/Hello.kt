53393138557bea5c5b64b7d5e31a143cd5f56847
Teodor Sveryda <tsveryda18@outlook.com>
2025-10-07 12:42:39 +0000

    finished_task2

--- tasks/task1_1/Hello.kt
+++ tasks/task1_1/Hello.kt
@@ -5,4 +5,4 @@ fun main() {
 
 
 // compile first by including "c" in "kotlinc app.kt
-//then run using kotlin appKt"
+//then run using kotlin AppKt" always use capital letter for the file name when running
